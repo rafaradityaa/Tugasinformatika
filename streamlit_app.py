d499ae0813a2a9f67bed52974a7d05a5a4bc00fa
rafaradityaa <rafaraditya0609@gmail.com>
2025-05-27 10:05:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.image("vit.jpg", width=200) 
+st.image("file_000000007ce061f7a22ac3d12b254176_conversation_id=67fbb908-a654-8013-9512-652d32bc7b5c&message_id=cfee5c1d-8cac-4ba7-bb49-1f42f7f90c1c.png", width=200) 
 
 
 st.title("aplikasi sederhana") 
